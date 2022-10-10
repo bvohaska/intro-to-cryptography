ee7fe13c6a202a86cbcbaad28a3ce3d13bd42739
bvohaska <bvohaska>
2022-10-09 23:41:11 -0400

    reorganizing the repo

--- cpa-challenge-homework/serverstart.py
+++ cpa-challenge-homework/serverstart.py
@@ -12,9 +12,3 @@ if __name__ == "__main__":
                ssl_keyfile="/etc/letsencrypt/live/ineedrandom.com/privkey.pem",
                ssl_certfile="/etc/letsencrypt/live/ineedrandom.com/fullchain.pem",
             )
-
-#uvicorn main:app 
-# --ssl-certfile /etc/letsencrypt/live/ineedrandom.com/fullchain.pem 
-# --ssl-keyfile /etc/letsencrypt/live/ineedrandom.com/privkey.pem 
-# --host 0.0.0.0  --port 443
-
